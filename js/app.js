6123d674d266c772d9fdcba83e79146cdcb97578
EdwardGoomba <ask@edwardvdesign.com>
2018-01-03 17:18:55 -0500

    Add interaction to button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-// Hide spoiler on page load
-$('.spoiler span').hide();
+$(document).ready( function() {
+  // Hide spoiler on page load
+  $('.spoiler span').hide();
 
-// Show spoiler text
+  // Show spoiler text
+  $('.spoiler button').click(function() {
+    $('.spoiler span').show();
+    $('.spoiler button').hide();
+  });
+
+});
